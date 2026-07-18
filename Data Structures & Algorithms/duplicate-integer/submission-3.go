87a327a3215ab1de78c4ec754757331116304797
Enorshionari <138007392+Enorshionari@users.noreply.github.com>
2026-07-18 20:54:53 +0300

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    m  := make(map[int]bool)
+    for _, i := range nums {
+        if m[i] {
+            return true
+        }
+        m[i] = true
+    }
+    return false
+}
